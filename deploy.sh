734928223d450fa2ee81e2db8077eda87312874a
Koichi Shiraishi <zcheeee@gmail.com>
2015-08-27 08:50:35 +0900

    Fix release message
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ set -x
 echo "Parse git log..."
 APP=`git log --oneline -1 | grep "\[" | awk '{print$2}' | sed 's/^\[\(.\+\)\/\(.\+\)\]/\1/g'`
 VERSION=`git log --oneline -1 | grep "\[" | awk '{print$2}' | sed 's/^\[\(.\+\)\/\(.\+\)\]/\2/g'`
-MESSAGE=`git log --oneline -1 | grep "\[" | awk '{$1="";$2="";$3="";$4="";print $0}' | sed 's/^   //g' | sed 's/  / /g'`
+MESSAGE=`git log --oneline -1 | grep "\[" | awk '{$1="";$2="";$3="";$5="";print $0}' | sed 's/^   //g' | sed 's/  / /g'`
 
 if [ $APP ]; then
   JAVA_VERSION=`sudo docker run -ti java java -version | awk 'NR==1 {print$0}'`
-  github-release release -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$MESSAGE" -d "Build by $JAVA_VERSION in Docker container"
+  github-release release -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$MESSAGE" -d "Build by $JAVA_VERSION"
   github-release upload -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$APP.jar" -f $APP.jar
 fi
