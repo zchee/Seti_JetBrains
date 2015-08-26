b34c4b595d8a3c1a9cde5d2eed9c7ab6998cbf72
Koichi Shiraishi <zcheeee@gmail.com>
2015-08-27 07:28:16 +0900

    [phpstorm/9.0.2] Upgrade PhpStorm to 9.0.2 build 141.2462
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ set -x
 echo "Parse git log..."
 APP=`git log --oneline -1 | grep "\[" | awk '{print$2}' | sed 's/^\[\(.\+\)\/\(.\+\)\]/\1/g'`
 VERSION=`git log --oneline -1 | grep "\[" | awk '{print$2}' | sed 's/^\[\(.\+\)\/\(.\+\)\]/\2/g'`
-MESSAGE=`git log --oneline -1 | grep "\[" | awk '{$1="";$2="";print $0}' | sed 's/^  //g'`
+MESSAGE=`git log --oneline -1 | grep "\[" | awk '{$1="";$2="";$3="";$5="";print $0}' | sed 's/^   //g' | sed 's/  / /g'`
 
 if [ $APP ]; then
-  github-release release -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$MESSAGE"
+  JAVA_VERSION1=`sudo docker run -ti java java -version | awk 'NR==1 {print$0}'`
+  JAVA_VERSION2=`sudo docker run -ti java java -version | awk 'NR==2 {print$0}'`
+  JAVA_VERSION3=`sudo docker run -ti java java -version | awk 'NR==3 {print$0}'`
+  github-release release -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$MESSAGE" -d "$JAVA_VERSION1\n$JAVA_VERSION2\n$JAVA_VERSION3"
   github-release upload -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$APP.jar" -f $APP.jar
 fi
