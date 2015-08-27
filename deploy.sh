33d690a0951566ab5f9dd904abb0092508a8e7bb
Koichi Shiraishi <zcheeee@gmail.com>
2015-08-27 09:23:35 +0900

    Fix release comment
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ MESSAGE=`git log --oneline -1 | grep "\[" | awk '{$1="";$2="";$3="";$5="";print 
 
 if [ $APP ]; then
   JAVA_VERSION=`sudo docker run -ti java java -version | awk 'NR==1 {print$0}'`
-  github-release release -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$MESSAGE" -d "Build by $JAVA_VERSION"
+  github-release release -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$MESSAGE" -d "Build in Docker container with $JAVA_VERSION"
   github-release upload -u zchee -r Seti_JetBrains -t "$APP/$VERSION" -n "$APP.jar" -f $APP.jar
 fi
