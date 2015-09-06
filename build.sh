efcdf4250096fc25db351d8b303d19be317c7d42
Koichi Shiraishi <zcheeee@gmail.com>
2015-09-06 21:45:45 +0900

    Move to directory
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ APP=`git log --oneline -1 | grep "\[" | awk '{print$2}' | sed 's/^\[\(.\+\)\/\(.
 VERSION=`git log --oneline -1 | grep "\[" | awk '{print$2}' | sed 's/^\[\(.\+\)\/\(.\+\)\]/\2/g'`
 
 if [ $APP ]; then
-  docker build -t $APP -f Dockerfile.$APP .
+  docker build -t $APP -f dockerfiles/Dockerfile.$APP .
   docker run $APP:latest > ./$APP.jar
 fi
 
